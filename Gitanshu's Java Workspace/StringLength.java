6358300183904e90a08cf8294bb40b0f8a74555c
Gitanshu2409 <86179904+Gitanshu2409@users.noreply.github.com>
2022-06-02 09:55:42 +0530

    Add files via upload

---
+++ Gitanshu's Java Workspace/StringLength.java
@@ -0,0 +1,10 @@
+// Index of first letter of a Word in a alloted String
+//
+public class StringLength {
+	public static void main(String[] args) {
+		// The lengthof alloted string can be calculated by Stringname.length() function.
+		String mybasicString = "Hi How are you doing.....";
+		System.out.println("This is the String : "+mybasicString);
+		System.out.println("The Length of the String is : "+mybasicString.length());
+	}
+}
---
+++ Gitanshu's Java Workspace/SwitchFunc.java
@@ -0,0 +1,27 @@
+public class SwitchFunc {
+   
+   /* The Switch function is faster than if else function and is less hectic 
+   to write it only needs an expression and performs the function according 
+   to its value in different cases without the hassle of writing the 
+   whole command again and again whereas every case or condition for if 
+   else command needs to entered specifically again and again everytime 
+   and the command works according to it.*/
+   
+   public static void main (String[]args) {
+      String myString = "a";
+   
+   switch (myString) {
+      
+      case "a" :
+      System.out.println ("'a' is the value of myString");
+      break;
+      
+      case "b" :
+      System.out.println ("'b' is the value of mystring");
+      break;
+      
+      }
+   }
+}
+   
+   
